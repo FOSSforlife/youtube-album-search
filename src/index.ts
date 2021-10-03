c1c5aad38ce963f005f9431e2ff1f65ab6e1f429
Elias Jackson <ejthedj0@gmail.com>
2021-10-03 10:34:11 -0700

    Implement YouTube API

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export function albumSearch(query: string) {
-	console.log(`album search ${query}`);
+import { youtube_v3 } from 'googleapis';
+
+// youtube_v3.Youtube
+export async function albumSearch(query: string, youtube: any) {
+	const results = await youtube.channels.list({
+		part: 'snippet,contentDetails,statistics',
+		forUsername: 'GoogleDevelopers',
+	});
+	return results;
 }
--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,15 @@
 // TODO: Delete this file once we have actual tests
 
+import { google } from 'googleapis';
 import { albumSearch } from '.';
+import dotenv from 'dotenv';
+dotenv.config();
 
-albumSearch('jane doe');
+(async () => {
+	const youtube = google.youtube({
+		version: 'v3',
+		auth: process.env.YOUTUBE_API_KEY,
+	});
+
+	console.log((await albumSearch('jane doe', youtube)).data.items[0]);
+})();
