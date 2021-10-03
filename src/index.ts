6d2175ecaba6f2bd73074943cd937aae0b76f7dd
Elias Jackson <ejthedj0@gmail.com>
2021-10-03 10:14:15 -0700

    Initial setup

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function albumSearch(query: string) {
+	console.log(`album search ${query}`);
+}
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+// TODO: Delete this file once we have actual tests
+
+import { albumSearch } from '.';
+
+albumSearch('jane doe');
